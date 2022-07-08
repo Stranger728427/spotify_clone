192e222f34b809f0e805ef017073360e34ce2864
Stranger <sk7674095@gmail.com>
2022-07-08 09:30:37 +0530

    add  new

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
 console.log("Welcome to Spotify");
-// initialize the variable
+
 let songIndex=0;
-let audioElement=new Audio('1.mp3');
+let audioElement=new Audio('ganda-gana.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
-let gif=document.getElementById('gif');
-let songItems=Array.from (document.getElementsByClassName('songItems'));
+// let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItems'));
 let songs=[
     
-    {songName: "Let Me Love You", filePath:"ganda-gana.mp3/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"}
+    {songName: "Let Me Love You", filePath:"ganda-gana.mp3",coverPath:"covers/1.jpg"},
+   // {songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
+    //{songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
+    //{songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"},
+    //{songName: "Let Me Love You", filePath:"E:\downloads/1.mp3",coverPath:"covers/1.jpg"}
 ]
 
 songItems.forEach((element,i)=>{
@@ -28,56 +27,24 @@ masterPlay.addEventListener('click',()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity=1;
+        // gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity=0;
+        // gif.style.opacity=0;
     }
     
 })
 
 //listen to event
-audioElement.myProgressBar.addEventListener('time' ,()=>{
-console.log('timeupdate');
-//update Seekbar
-progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
-console.log(progress);
-myProgressBar.value=progress;
-
-})
- myProgressBar.addEventListener('change',()=>{
-    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
- })
-=======
-console.log("Welcome to Spotify");
-// initialize the variable
-let songIndex=0;
-let audioElement=new Audio('1.mp3');
-let masterPlay=document.getElementById('masterPlay');
-let myProgressBar=document.getElementById('myProgressBar');
-let songs=[
-    
-    {songName: "Let Me Love You", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Let Me Love You", filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-]
-
-
-//handle play/pause click
-masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-    }
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
 })
 
-//listen to event
-myProgressBar.addEventListener('time',()=>{
-console.log('timeupdate');
-//update Seekbar
-
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
 })
->>>>>>> 717d5fa425f9afbc935e046a3e714d6cacd4cca2
